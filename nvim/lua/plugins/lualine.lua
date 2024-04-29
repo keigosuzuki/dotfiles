3b5d6ac2d54d47e3667e1ed1bac66e9c57f05e8e
21suzuki <suzuki-keigo093@g.ecc.u-tokyo.ac.jp>
2024-04-29 22:42:50 +0900

    add fileformat symbols

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -26,7 +26,18 @@ require('lualine').setup {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
         lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_x = {
+            'encoding', 
+            {
+                'fileformat',
+                symbols = {
+                    unix = '',
+                    dos = '',
+                    mac = '',
+                }
+            },
+            'filetype'
+        },
         lualine_y = {'progress'},
         lualine_z = {'location'}
     },
