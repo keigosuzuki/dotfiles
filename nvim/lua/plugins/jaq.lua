c5dcf73687884f6ac9235cd93a0232503c60c6b8
21suzuki <suzuki-keigo093@g.ecc.u-tokyo.ac.jp>
2024-05-07 15:57:12 +0900

    marp with jaq

--- nvim/lua/plugins/jaq.lua
+++ nvim/lua/plugins/jaq.lua
@@ -1,7 +1,8 @@
 require('jaq-nvim').setup{
   cmds = {
     external = {
-      tex = "latexmk -pv % && latexmk -c %"
+      tex = "latexmk -pv % && latexmk -c %",
+      markdown = "marp --pdf % --html --allow-local-files --image-scale 2 && open $fileBase.pdf"
     }
   },
 
